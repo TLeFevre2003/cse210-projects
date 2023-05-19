848b8be4cadc01f095e180dbeb23807b8dc9fb19
Tyler LeFevre <tml20500@gmail.com>
2023-05-19 11:24:29 -0600

    Finished Learning03 fractions program

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,25 @@
 using System;
-
-class Program
+namespace Learning03
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning03 World!");
+        static void Main(string[] args)
+        {
+            Fraction firstFraction = new Fraction();
+            Console.WriteLine(firstFraction.GetFractionString());
+            Console.WriteLine(firstFraction.GetDecimalValue());
+
+            Fraction secondFraction = new Fraction(5,1);
+            Console.WriteLine(secondFraction.GetFractionString());
+            Console.WriteLine(secondFraction.GetDecimalValue());
+
+            Fraction thirdFraction = new Fraction(3,4);
+            Console.WriteLine(thirdFraction.GetFractionString());
+            Console.WriteLine(thirdFraction.GetDecimalValue());
+
+            Fraction forthFraction = new Fraction(1,3);
+            Console.WriteLine(forthFraction.GetFractionString());
+            Console.WriteLine(forthFraction.GetDecimalValue());
+        }
     }
 }
