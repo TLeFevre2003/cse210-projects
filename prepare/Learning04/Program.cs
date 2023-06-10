6de5ddbe6904a80fc32781adcbe90b34ada0cd53
Tyler LeFevre <tml20500@gmail.com>
2023-06-10 13:48:09 -0600

    Finished learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,20 @@
 using System;
-
-class Program
+namespace Learning04
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning04 World!");
+        static void Main(string[] args)
+        {
+            Assignment newAssignment = new Assignment("Samuel Bennett","Multiplication");
+            Console.WriteLine(newAssignment.GetSummary());
+
+            MathAssignment newMath = new MathAssignment("Roberto Rodriguez","Fractions","7.3","8-19");
+            Console.WriteLine(newMath.GetSummary());
+            Console.WriteLine(newMath.GetHomeworkList());
+            
+            WritingAssignment newWriting = new WritingAssignment("Mary Waters","European History","The Causes of World War II by Mary Waters");
+            Console.WriteLine(newWriting.GetSummary());
+            Console.WriteLine(newWriting.GetWritingInformation());
+        }
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,29 @@
+using System;
+namespace Learning04
+{
+    class WritingAssignment : Assignment
+    {
+
+        private string _title;
+
+        public WritingAssignment (string name, string topic,string title) : base(name,topic)
+        {
+
+
+            _title = title;
+
+
+
+        }
+        public string GetWritingInformation()
+        {
+            return _title;
+        }
+
+    }
+
+
+
+
+
+}
