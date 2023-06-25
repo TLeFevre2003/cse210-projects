8f0b0332a3bba45cd257311c675641f8bece4a03
Tyler LeFevre <tml20500@gmail.com>
2023-06-24 22:48:30 -0600

    Finished Polymophism learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,29 @@
 using System;
-
-class Program
+using System.Collections.Generic;
+namespace Learning05
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning05 World!");
+        static void Main(string[] args)
+        {
+            Square mySquare = new Square("red",20);
+
+            Rectangle myRectangle = new Rectangle("red",10,5);
+
+            Circle myCircle = new Circle("red",11);
+
+            List<Shape> shapes = new List<Shape>
+            {
+                mySquare,
+                myRectangle,
+                myCircle
+            };
+            foreach (Shape shape in shapes)
+            {
+                Console.WriteLine($"The color is {shape.GetColor()} the area is {shape.GetArea()}");
+
+
+            }
+        }
     }
 }
